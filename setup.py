756d1460c59919c05f8b2d9e5821b2243dc9a4d6
Stephen Martin <lockwood@opperline.com>
2018-10-19 18:36:13 -0700

    upgrade tox to 3.5.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
             'pytest==3.8.0',
             'pytest_asyncio==0.9.0',
             'pytest_httpbin==0.3.0',
-            'tox==3.3.0'
+            'tox==3.5.2'
         ]
     },
     entry_points={
