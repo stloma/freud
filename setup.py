9b1b74402dcd3cce650ba19ea326d0c1f0acbc1f
Stephen Martin <lockwood@opperline.com>
2019-01-10 10:10:23 -0800

    upgrade requests to 2.21.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     keywords='cli tui http rest request api',
     packages=find_packages(),
     install_requires=[
-        'requests==2.20.0',
+        'requests==2.21.0',
         'prompt_toolkit==2.0.7',
         'pygments==2.2.0'
     ],
