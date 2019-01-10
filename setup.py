160e8ea8008ce39c5dcda6f3ddc59898f20384c4
Stephen Martin <lockwood@opperline.com>
2019-01-10 10:11:07 -0800

    upgrade pytest to 4.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     extras_require={
         'dev': [
             'flake8==3.6.0',
-            'pytest==3.9.3',
+            'pytest==4.1.0',
             'pytest_asyncio==0.10.0',
             'pytest_httpbin==0.3.0',
             'tox==3.5.3'
